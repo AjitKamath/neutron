36e27503d97bb76462a611edff23588c9cfbc9d4
Ajit Kamath <ajitkamathk@gmail.com>
2016-03-01 16:45:10 +0530

    Commit from office 1 feb 2016 - 2
    code clean up

--- app/src/main/java/com/finappl/utils/DateTimeUtil.java
+++ app/src/main/java/com/finappl/utils/DateTimeUtil.java
@@ -51,58 +51,42 @@ public class DateTimeUtil {
 		
 		//get Start Date
 		Calendar cal = Calendar.getInstance();
-    	cal.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
-    	cal.clear(Calendar.MINUTE);
-	    cal.clear(Calendar.SECOND);
-	    cal.clear(Calendar.MILLISECOND);
+	    	cal.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+	    	cal.clear(Calendar.MINUTE);
+		cal.clear(Calendar.SECOND);
+		cal.clear(Calendar.MILLISECOND);
+		    	
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.MONTH, month);
+		cal.set(Calendar.YEAR, year);
+		    
+		cal.add(Calendar.MONTH, -range);
+		String startDayStr = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
+		String startMonthStr = String.valueOf(cal.get(Calendar.MONTH)+1);
+		String startYearStr = String.valueOf(cal.get(Calendar.YEAR));
+		//get Start Date ends
 	    	
-	    cal.set(Calendar.DAY_OF_MONTH, 1);
-	    cal.set(Calendar.MONTH, month);
-	    cal.set(Calendar.YEAR, year);
-	    
-	    cal.add(Calendar.MONTH, -range);
-	    String startDayStr = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
-	    String startMonthStr = String.valueOf(cal.get(Calendar.MONTH)+1);
-	    String startYearStr = String.valueOf(cal.get(Calendar.YEAR));
-	    //get Start Date ends
-    	
-    	//get End Date
+	    	//get End Date
 		cal = Calendar.getInstance();
-	    cal.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
-	    cal.clear(Calendar.MINUTE);
-	    cal.clear(Calendar.SECOND);
-	    cal.clear(Calendar.MILLISECOND);
+		cal.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		cal.clear(Calendar.MINUTE);
+		cal.clear(Calendar.SECOND);
+		cal.clear(Calendar.MILLISECOND);
+		   	
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.MONTH, month);
+		cal.set(Calendar.YEAR, year);
+		   
+		cal.add(Calendar.MONTH, +range+1);
+		cal.add(Calendar.DAY_OF_MONTH, -1);
+		String endDayStr = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
+		String endMonthStr = String.valueOf(cal.get(Calendar.MONTH)+1);
+		String endYearStr = String.valueOf(cal.get(Calendar.YEAR));
+		//get End Date ends
 	    	
-	    cal.set(Calendar.DAY_OF_MONTH, 1);
-	    cal.set(Calendar.MONTH, month);
-	    cal.set(Calendar.YEAR, year);
-	    
-	    cal.add(Calendar.MONTH, +range+1);
-	    cal.add(Calendar.DAY_OF_MONTH, -1);
-	    String endDayStr = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
-	    String endMonthStr = String.valueOf(cal.get(Calendar.MONTH)+1);
-	    String endYearStr = String.valueOf(cal.get(Calendar.YEAR));
-	    //get End Date ends
-    	
-    	if(startDayStr.length() != 2){
-	    	startDayStr = "0"+startDayStr;
-	    }
-	    	
-	    if(startMonthStr.length() != 2){
-	    	startMonthStr = "0"+startMonthStr;
-	    }
-	    	
-	    if(endDayStr.length() != 2){
-	    	endDayStr = "0"+endDayStr;
-	    }
-	    	
-	    if(endMonthStr.length() != 2){
-	    	endMonthStr = "0"+endMonthStr;
-	    }
-	    	
-	    String startDateStr =  startDayStr+"-"+startMonthStr+"-"+startYearStr;
-	    String endDateStr = endDayStr+"-"+endMonthStr+"-"+endYearStr;
-	    return new String[]{startDateStr, endDateStr};
+	    	String startDateStr =  reformatDate(startDayStr+"-"+startMonthStr+"-"+startYearStr);
+		String endDateStr = reformatDate(endDayStr+"-"+endMonthStr+"-"+endYearStr);
+		return new String[]{startDateStr, endDateStr};
 	}
 	
 	public boolean isDateAfterOrEquals(String checkDateStr, String withDateStr){
